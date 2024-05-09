cd4c3cf5ffd910cdb72e00ff24751b1a252ff0c2
Marcos Daniel <130110150+marcosprog-cmd@users.noreply.github.com>
2024-05-09 07:59:41 -0300

    Update script.js

--- script.js
+++ script.js
@@ -63,3 +63,22 @@ function createFileLink(pdf) {
     fileLink.setAttribute('target', '_blank');
     return fileLink;
 }
+
+window.onload = function() {
+    
+    var modal = document.getElementById("myModal");
+    modal.style.display = "block";
+  
+   
+    var span = document.getElementsByClassName("close")[0];
+    span.onclick = function() {
+      modal.style.display = "none";
+    }
+  
+    
+    window.onclick = function(event) {
+      if (event.target == modal) {
+        modal.style.display = "none";
+      }
+    }
+  }
